8114d879769259cb3f6378a3ab85a4a1696663ee
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2022-12-12 13:54:15 +0200

    update tweet model

--- server/models/requests/tweet.py
+++ server/models/requests/tweet.py
@@ -13,9 +13,9 @@ class TweetReq(BaseModel):
     - end_at: [int] epoch millisecond
     """
     text: str
-    top_left_lat: Optional[float] = 40.73
-    top_left_lon: Optional[float] = -108.35883
-    bottom_right_lat: Optional[float] = 40.01
-    bottom_right_lon: Optional[float] = -101.31067
+    top_left_lat: Optional[float] = 90
+    top_left_lon: Optional[float] = -180
+    bottom_right_lat: Optional[float] = -90
+    bottom_right_lon: Optional[float] = 180
     start_at: Optional[str] = "2013-09-08T10:45:32.038Z"
     end_at: Optional[str] = "2013-12-30T20:47:46.019Z"
